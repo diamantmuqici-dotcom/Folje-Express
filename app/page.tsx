a869675efe7ba593454fd94cb1cb824c62934f21
Sharritech <diamantmuqici@gmail.com>
2026-09-23 18:42:23 +0200

    Add FOLJE EXPRESS website and admin UI

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { readDesigns } from "@/lib/store";
+import HomeClient from "./home-client";
+
+export const dynamic="force-dynamic";
+
+export default async function Page(){
+  const designs=(await readDesigns()).filter(d=>d.visible);
+  return <HomeClient initialDesigns={designs}/>;
+}
